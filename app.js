48c9ee7963f95f3c6cf8a1653217133a10048cb0
Rabby-sopno <sopno01737@gmail.com>
2020-08-18 22:51:49 +0600

    cleaning code completed

--- app.js
+++ app.js
@@ -8,44 +8,57 @@ const showingResults = (receivedData, titles, singers, buttons) => {
     const songTitle = titles[i];
     const singer = singers[i];
     const lyricsButton = buttons[i];
-
     songTitle.innerText = receivedData.data[i].title;
     singer.innerText = receivedData.data[i].artist.name;
-
     lyricsButton.setAttribute("data-title", `${songTitle.innerText}`);
     lyricsButton.setAttribute("data-singer", `${singer.innerText}`);
   }
 };
 
 //* showing lyrics
-const showingLyrics = (singer, title, lyricsContainer, heading) => {
-  fetch(`https://api.lyrics.ovh/v1/${singer}/${title}`)
-    .then((response) => response.json())
+const showingLyrics = async (singer, title, lyricsContainer, heading) => {
+  const response = await fetch(`https://api.lyrics.ovh/v1/${singer}/${title}`)
+    .then((response) => {
+      if (!response.ok) throw new Error("Lyrics not found");
+      return response.json();
+    })
     .then((data) => {
       lyricsContainer.innerText = data.lyrics;
       heading.innerText = `${title}--${singer}`;
-    });
+    })
+    .catch((error) => alert(error));
 };
 
-searchBtn.addEventListener("click", () => {
-  const searchValue = searchBox.value;
-  //*simple results declaration
-  const songTitles = document.querySelectorAll(".song_title");
-  const singers = document.querySelectorAll(".singer");
-  const lyricsButtons = document.querySelectorAll(".getting_lyrics_btn");
-  //* fancy results declaretion
-  const fancyLyricsButtons = document.querySelectorAll(".fancy_lyrics_button");
-  const fancyLyricsNames = document.querySelectorAll(".fancy_lyrics_names");
-  const fancySingers = document.querySelectorAll(".fancy_singers");
+//* searching result
+const searchResult = (() => {
+  searchBtn.addEventListener("click", () => {
+    if (!searchBox.value) return alert("please type any song or singers name");
+    //*simple results declaration
+    const songTitles = document.querySelectorAll(".song_title");
+    const singers = document.querySelectorAll(".singer");
+    const lyricsButtons = document.querySelectorAll(".getting_lyrics_btn");
+    //* fancy results declaretion
+    const fancyLyricsButtons = document.querySelectorAll(
+      ".fancy_lyrics_button"
+    );
+    const fancyLyricsNames = document.querySelectorAll(".fancy_lyrics_names");
+    const fancySingers = document.querySelectorAll(".fancy_singers");
 
-  fetch(`https://api.lyrics.ovh/suggest/${searchValue}`)
-    .then((response) => response.json())
-    .then((data) => {
-      showingResults(data, songTitles, singers, lyricsButtons);
-      showingResults(data, fancyLyricsNames, fancySingers, fancyLyricsButtons);
-    });
-});
+    fetch(`https://api.lyrics.ovh/suggest/${searchValue}`)
+      .then((response) => response.json())
+      .then((data) => {
+        showingResults(data, songTitles, singers, lyricsButtons);
+        showingResults(
+          data,
+          fancyLyricsNames,
+          fancySingers,
+          fancyLyricsButtons
+        );
+      });
+  });
+})();
 
+//* simple results handeling
 simpleBtn.addEventListener("click", (e) => {
   const targetBtn = e.target;
   if (!targetBtn.matches("button")) return;
@@ -56,34 +69,27 @@ simpleBtn.addEventListener("click", (e) => {
   showingLyrics(dataSingerAttribute, dataTitleAttribute, lyrics, lyricsHeading);
 });
 
-//*/ fancy result started
-const fancyButton = document.querySelector("#fancy_button");
-const fancyLyricsButtons = document.querySelectorAll(".fancy_lyrics_button");
-const fancyLyricsNames = document.querySelectorAll(".fancy_lyrics_names");
-const fancySingers = document.querySelectorAll(".fancy_singers");
-const fancyLyricsHeading = document.querySelectorAll(".fancy_lyrics_heading");
-const fancyLyrics = document.querySelectorAll(".fancy_lyrics");
+//*/ fancy results handeling
+const fancyLyricsGenerator = (() => {
+  const fancyLyricsButtons = document.querySelectorAll(".fancy_lyrics_button");
+  const fancyLyricsHeading = document.querySelectorAll(".fancy_lyrics_heading");
+  const fancyLyrics = document.querySelectorAll(".fancy_lyrics");
 
-// console.log(fancyButton);
-// console.log(fancyLyricsButtons);
-// console.log(fancyLyricsNames);
-// console.log(fancySingers);
-// console.log(fancyLyricsHeading);
-// console.log(fancyLyrics);
+  for (let i = 0; i < fancyLyricsButtons.length; i++) {
+    const fancyLyricsButton = fancyLyricsButtons[i];
+    const fancySingleLyrics = fancyLyrics[i];
+    const fancySingleHeading = fancyLyricsHeading[i];
 
-for (let i = 0; i < fancyLyricsButtons.length; i++) {
-  const fancyLyricsButton = fancyLyricsButtons[i];
-  const fancySingleLyrics = fancyLyrics[i];
-  const fancySingleHeading = fancyLyricsHeading[i];
-  fancyLyricsButton.addEventListener("click", (e) => {
-    const targetButton = e.target;
-    const fancyTitle = targetButton.getAttribute("data-title");
-    const fancySinger = targetButton.getAttribute("data-singer");
-    showingLyrics(
-      fancySinger,
-      fancyTitle,
-      fancySingleLyrics,
-      fancySingleHeading
-    );
-  });
-}
+    fancyLyricsButton.addEventListener("click", (e) => {
+      const targetButton = e.target;
+      const fancyTitle = targetButton.getAttribute("data-title");
+      const fancySinger = targetButton.getAttribute("data-singer");
+      showingLyrics(
+        fancySinger,
+        fancyTitle,
+        fancySingleLyrics,
+        fancySingleHeading
+      );
+    });
+  }
+})();
